c5623288a5206415c4c1a629b2ab1eb65c5653e0
Marco Martínez <marcomar3104gaming@gmail.com>
2023-12-06 19:22:44 +0100

    PSP: P3) He añadido FirebaseAuth al programa. He añadido los métodos para iniciar sesión, registrar un nuevo usuario, y cerrar la saesión al FirebaseAdmin.

--- lib/Singletone/FirebaseAdmin.dart
+++ lib/Singletone/FirebaseAdmin.dart
@@ -1,3 +1,40 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
 class FirebaseAdmin {
-  
+  Future<void> registerUsuario(BuildContext _context, String emailAddress, String password) async {
+    try {
+      await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: emailAddress,
+        password: password,
+      );
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        print('The password provided is too weak.');
+      } else if (e.code == 'email-already-in-use') {
+        print('The account already exists for that email.');
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future<void> logInUsuario(String emailAddress, String password) async {
+    try {
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+          email: emailAddress,
+          password: password
+      );
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        print('No user found for that email.');
+      } else if (e.code == 'wrong-password') {
+        print('Wrong password provided for that user.');
+      }
+    }
+  }
+
+  Future<void> logOutUsuario() async {
+    await FirebaseAuth.instance.signOut();
+  }
 }
